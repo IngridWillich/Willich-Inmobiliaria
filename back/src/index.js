2920447aed024b479cc368aa804016aa5ab1b4a5
IngridWillich <ingridwillich@gmail.com>
2024-05-01 01:30:42 -0300

    hw2 m3

---
+++ back/src/index.js
@@ -0,0 +1,4 @@
+var n1 = 10;
+var n2 = 20;
+var sumar = function (a, b) { return (a + b); };
+console.log(sumar(n1, n2));
---
+++ back/src/index.ts
@@ -0,0 +1,5 @@
+const n1: number = 10;
+const n2: number = 20;
+
+const sumar = (a: number, b: number): number => (a + b);
+console.log(sumar(n1, n2)); 
