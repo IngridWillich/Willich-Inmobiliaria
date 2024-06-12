03a7d7d518ef59bc367f0a048c99e318c5d4bc8e
IngridWillich <ingridwillich@gmail.com>
2024-06-11 21:47:21 -0300

    Back funcionando

--- back/src/bd/appointments.ts
+++ back/src/bd/appointments.ts
@@ -1,5 +1,5 @@
 export const appointments=[
     { id: 1, date: '2023-05-01', client: 'John Doe' },
     { id: 2, date: '2023-05-02', client: 'Jane Doe' },
-    
-];
+   {  id: 3, name: "Appointment 3", date: "2024-06-12" },
+ ]
